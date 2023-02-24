43ead2d0ae4ad897f4e75522269a771006f71491
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-02-24 12:40:54 +0200

    Add game engine

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 let state = initState();
 let game = initGameObjects();
 
-
 game.startScreen.addEventListener('click', (e) => {
+
     game.startScreen.classList.add('hidden');
     game.gameScreen.classList.remove('hidden');
+
+    // start game
+    start(state, game);
 });
 
 
